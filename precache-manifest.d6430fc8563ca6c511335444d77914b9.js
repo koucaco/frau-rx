8e4715ab4c765000f225f57b58a073a1e867dce0
koucaco <hwang.ch@husky.neu.edu>
2020-01-21 15:51:06 -0500

    Updates

---
+++ precache-manifest.d6430fc8563ca6c511335444d77914b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630945c53924646251212f40c81aec60",
+    "url": "/frau-rx/index.html"
+  },
+  {
+    "revision": "cd479aa56939b9be7197",
+    "url": "/frau-rx/static/css/main.0a5d7651.chunk.css"
+  },
+  {
+    "revision": "30561554d4133670291f",
+    "url": "/frau-rx/static/js/2.c07c04a3.chunk.js"
+  },
+  {
+    "revision": "bd58a53ea30c71d23a62143160dc052c",
+    "url": "/frau-rx/static/js/2.c07c04a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd479aa56939b9be7197",
+    "url": "/frau-rx/static/js/main.e1eb1686.chunk.js"
+  },
+  {
+    "revision": "ad3478ffa11b104867d1",
+    "url": "/frau-rx/static/js/runtime-main.2d9392ce.js"
+  }
+]);
